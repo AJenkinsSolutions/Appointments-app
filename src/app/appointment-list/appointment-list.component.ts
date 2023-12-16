ddb90f3659f668e065b0a75806daca2f630afeac
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2023-12-16 08:44:47 -0600

    Add: Two way databinding

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
 
+
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
 export class AppointmentListComponent {
+
+  //Input fields
+  newAppointmentTitle: string = "";
+  newAppointmentDate: Date = new Date();
+
+
 
   //List of appointments
   appointment: Appointment = {
