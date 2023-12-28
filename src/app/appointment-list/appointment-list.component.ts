816721742716be31cea2bffa5593fe6d74d3180e
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2023-12-28 10:22:32 -0700

    Add delete appointment functionality

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -13,19 +13,51 @@ export class AppointmentListComponent {
   newAppointmentTitle: string = "";
   newAppointmentDate: Date = new Date();
 
+  clicked: boolean = false;
+
   addAppointment(){
     console.log("INFO: Entered addAppointment method in AppointmentListComponent class");
 
-    alert(this.newAppointmentTitle + " " + this.newAppointmentDate);
+    //Conditions for adding new appointment
+    if(this.newAppointmentTitle.length > 0){
+
+      //Creating new ObJ
+      let newAppointment: Appointment = {
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate
+  
+      };
+      //Add to list
+      this.appointmentsList.push(newAppointment);
+
+      //Clean up
+      this.newAppointmentTitle = "";
+      this.newAppointmentDate = new Date();
+
+    }
+
+    this.clicked = true;
 
   }
 
+  deleteAppointment(index: number){
+    this.appointmentsList.splice(index, 1);
+
+  }
+  
+
+
+  //Defualt Appointment Model
+  // appointment: Appointment = {
+  //   id: 1,
+  //   title: "Default appointment",
+  //   date: new Date(Date.now())}
 
 
   //List of appointments
-  appointment: Appointment = {
-    id: 1,
-    title: "a",
-    date: new Date('2023-07-30')}
+  appointmentsList: Appointment[] = [];
 
 }
+
+
